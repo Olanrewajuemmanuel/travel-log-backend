e6d0b7addaacea4c525274a7df777ba5fe25e7b8
Olanrewajuemmanuel <63521462+Olanrewajuemmanuel@users.noreply.github.com>
2022-08-26 15:09:24 +0100

    - added request limit - removed static path

--- src/index.ts
+++ src/index.ts
@@ -21,9 +21,8 @@ const PORT = process.env.PORT || 4000;
 async function initializeApp(app: Express) {
   // middlewares
   app.use(morgan("dev"));
-  app.use(express.urlencoded({ extended: true }));
+  app.use(express.urlencoded({ extended: true, limit: '5mb' }));
   app.use(express.json());
-  app.use(express.static(path.join(__dirname, "uploads")))
 
   app.use(cors());
   app.use(helmet());
