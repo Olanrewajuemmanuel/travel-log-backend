edb73b97384ee2a1cf001ea7879cedf7a8f422c4
Olanrewajuemmanuel <63521462+Olanrewajuemmanuel@users.noreply.github.com>
2022-09-12 19:53:16 +0100

    - deployment preparations

--- src/index.ts
+++ src/index.ts
@@ -8,44 +8,61 @@ import userRouter from "./routes/auth";
 import feedRouter from "./routes/feed";
 import profileRouter from "./routes/profile";
 import multer from "multer";
-import cookieParser from "cookie-parser"
-import {expressjwt} from "express-jwt"
+import cookieParser from "cookie-parser";
+import { expressjwt } from "express-jwt";
+import path from "path";
 
 dotenv.config();
 
 const app: Express = express();
 
 const PORT = process.env.PORT || 4000;
-
-
+const isInProduction =
+  process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging";
 
 async function initializeApp(app: Express) {
+  if (isInProduction) {
+    app.use(express.static("frontend/build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "frontend/build/index.html"));
+    });
+  }
   // middlewares
-  app.use(morgan("dev"));
-  app.use(express.urlencoded({ extended: true, limit: '5mb' }));
+  app.use(morgan(`${isInProduction ? "combined" : "dev"}`));
+  app.use(express.urlencoded({ extended: true, limit: "5mb" }));
   app.use(express.json());
-  app.use(express.static("uploads"))
+  app.use(express.static("uploads"));
 
-  app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-  }));
+  app.use(
+    cors({
+      origin: "http://localhost:3000/",
+      credentials: true,
+    })
+  );
   app.use(helmet());
-  app.use(cookieParser())
-  app.use(expressjwt({
-    secret: process.env.JWT_SECRET as string,
-    algorithms: ['HS256'],
-    getToken: (req: any) => {
-      return req.cookies.accessToken
-    },
-  }).unless({ path: ['/user/login', '/user/register', '/user/refresh', '/user/logout'] })
-  )
+  app.use(cookieParser());
+  app.use(
+    expressjwt({
+      secret: process.env.JWT_SECRET as string,
+      algorithms: ["HS256"],
+      getToken: (req: any) => {
+        return req.cookies.accessToken;
+      },
+    }).unless({
+      path: [
+        "/api/user/login",
+        "/api/user/register",
+        "/api/user/refresh",
+        "/api/user/logout",
+      ],
+    })
+  );
   app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     if (err instanceof multer.MulterError) {
       if (err.code === "LIMIT_FILE_SIZE") {
         return res.json({
-          message: "file is too large, max_size: 2MB"
-        })
+          message: "file is too large, max_size: 2MB",
+        });
       }
     }
     res.status(500);
@@ -53,9 +70,9 @@ async function initializeApp(app: Express) {
   });
 
   //routes
-  app.use("/user", userRouter);
-  app.use("/feed", feedRouter);
-  app.use("/profile", profileRouter);
+  app.use("/api/user", userRouter);
+  app.use("/api/feed", feedRouter);
+  app.use("/api/profile", profileRouter);
 
   // connect to mongoDB
   try {
@@ -63,8 +80,8 @@ async function initializeApp(app: Express) {
     if (connection) {
       return Promise.resolve("--- Mongo DB is connected ---");
     }
-  } catch(err) {
-    return Promise.reject(err)
+  } catch (err) {
+    return Promise.reject(err);
   }
 }
 // TODO: Try, catch block for this!!
